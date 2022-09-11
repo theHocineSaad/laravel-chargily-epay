098524974f7bdd45913fda78b108611ec6336bda
thehocinesaad <saadhocine0@gmail.com>
2022-09-11 14:00:47 +0100

    gave a name to a route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use TheHocineSaad\LaravelChargilyEPay\Http\Controllers\EpayWebhookController;
 
 Route::middleware(['web'])->group(function () {
-    Route::get('/epay-webhook-tester', [EpayWebhookController::class, 'test']);
+    Route::get('/epay-webhook-tester', [EpayWebhookController::class, 'test'])->name('epay_webhook_tester.get');
     Route::post('/epay-webhook-tester', [EpayWebhookController::class, 'test_store'])->name('epay_webhook_tester.post');
 });
