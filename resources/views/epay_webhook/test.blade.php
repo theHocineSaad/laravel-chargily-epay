4ed8b3ff0131af6cbeca85ca826085b682e82b90
thehocinesaad <saadhocine0@gmail.com>
2022-09-11 10:18:39 +0100

    added noindex meta tag

--- resources/views/epay_webhook/test.blade.php
+++ resources/views/epay_webhook/test.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex">
     <title>Epay Webhook Tester</title>
 
     <style>
